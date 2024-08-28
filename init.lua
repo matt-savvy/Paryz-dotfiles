1b9433bcaf9cc186c32228304e67d94631a96697
Matt Savoia <matt@1-800-rad-dude.com>
2024-08-28 19:15:04 -0400

    Add laststatus

--- init.lua
+++ init.lua
@@ -3,6 +3,7 @@ vim.cmd.source("~/dotfiles/vimrc")
 vim.opt.hidden = true           -- Allow buffers to become hidden
 vim.opt.encoding = "utf-8"      -- Use UTF-8 encoding
 vim.opt.incsearch = true        -- Move while searching
+vim.opt.laststatus = 2          -- Always show the status line
 
 ------------- Color Schemes ----------------
 vim.opt.termguicolors = true
