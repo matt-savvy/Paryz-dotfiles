897e0407c705615dc1ccedcfe9caa69c881ff03c
Matt Savoia <matt@1-800-rad-dude.com>
2024-08-28 19:16:21 -0400

    Add default comments

--- init.lua
+++ init.lua
@@ -1,9 +1,10 @@
 vim.cmd.source("~/dotfiles/vimrc")
 
-vim.opt.hidden = true           -- Allow buffers to become hidden
-vim.opt.encoding = "utf-8"      -- Use UTF-8 encoding
-vim.opt.incsearch = true        -- Move while searching
-vim.opt.laststatus = 2          -- Always show the status line
+vim.opt.hidden = true           -- Allow buffers to become hidden (default)
+vim.opt.encoding = "utf-8"      -- Use UTF-8 encoding (default)
+vim.opt.incsearch = true        -- Move while searching (default)
+vim.opt.laststatus = 2          -- Always show the status line (default)
+
 
 ------------- Color Schemes ----------------
 vim.opt.termguicolors = true
